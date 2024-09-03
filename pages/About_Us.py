1b1f84e6c848efa558ad7f709172cb0bbab28d29
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2024-09-03 14:39:08 +0300

    requirement changes2

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -2,7 +2,7 @@ import streamlit as st
 from constants.constants import project_name, my_layout, my_menu_items
 from constants.functions import page_header_infos
 
-page_header_infos(title='About Us')
+page_header_infos(title='About Us and Data')
 
 
 
